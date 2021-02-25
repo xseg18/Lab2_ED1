5b2cb059790b13385532c492f884759c44e1d96a
Ximena Elizardi <ximenagobern@gmail.com>
2021-02-24 21:14:28 -0600

    Class and menu
    Clase medicamento creada y cambios visuales en el menú

---
+++ Models/Medicine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_ED1.Models
+{
+    public class Medicine
+    {
+        public static int ID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string HManufact { get; set; }
+        public int Qty  { get; set; }
+    }
+}
