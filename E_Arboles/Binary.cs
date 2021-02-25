3764f0ff31c1b31e13ef256513b8b898dbb9068e
Ximena Elizardi <ximenagobern@gmail.com>
2021-02-24 20:43:28 -0600

    List and tree files added
    archivos de libreria de clases añadidos

---
+++ E_Arboles/Binary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace E_Arboles
+{
+    public class Binary<T>
+    {
+        class Node
+        {
+            Node Right;
+            Node Left;
+            T data; 
+        }
+        Node Root;
+    }
+}
