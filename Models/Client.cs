38f62fc1221dd21c083e45a85fae4716bcd2282c
Ximena Elizardi <ximenagobern@gmail.com>
2021-02-26 10:28:59 -0600

    Client model added

---
+++ Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_ED1.Models
+{
+    public class Client
+    {
+        public string name { get; set; }
+        public string Adress { get; set; }
+        public int NIT { get; set; }
+        public int Total { get; set; }
+        public ELineales.Lista<Medicine> medicines { get; set; }
+        
+       
+
+    }
+}
